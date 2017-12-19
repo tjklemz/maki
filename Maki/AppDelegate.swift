f9a7b9c5bc3ca212bba65ed29954ae32b35eaafa
Thomas Klemz <tjklemz@gmail.com>
2017-12-19 14:10:51 -0800

    Initial Commit

---
+++ Maki/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Maki
+//
+//  Created by Thomas Klemz on 12/19/17.
+//  Copyright © 2017 Lory & Ludlow. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Maki/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Maki
+//
+//  Created by Thomas Klemz on 12/19/17.
+//  Copyright © 2017 Lory & Ludlow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
